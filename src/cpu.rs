4899b8c790eb81e955f6871cfc94df3ba1a6c38a
luexp <10457192@qq.com>
2024-04-02 23:48:27 +0800

    feat: reset

--- src/cpu.rs
+++ src/cpu.rs
@@ -58,14 +58,36 @@ impl CPU {
         self.memory[addr as usize] = data;
     }
 
+    fn mem_read_u16(&self, pos:u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos+1) as u16;
+        (hi << 8) | lo
+    }
+
+    fn mem_write_u16(&mut self, pos:u16, data:u16) {
+        let hi = (data > 8) as u8;
+        let lo = (data & 0xFF) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos+1, hi);
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
     pub fn load_and_run(&mut self, program: Vec<u8>) {
         self.load(program);
+        self.reset();
         self.run();
     }
 
     pub fn load(&mut self, program: Vec<u8>) {
         self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
-        self.program_counter = 0x8000;
+        self.mem_write_u16(0xFFFC, 0x8000);
     }
 
     pub fn run(&mut self) {
