3e39765e84b731ab5f4f98a9a52d1dbb86fbba0b
luexp <10457192@qq.com>
2024-04-04 00:29:05 +0800

    feat: impl adc enum

--- src/asm.rs
+++ src/asm.rs
@@ -14,20 +14,23 @@ pub enum AddressingMode {
 }
 
 pub struct OpCode {
+    pub code: u8,
     pub len: u8,
     pub cycles: u8,
     pub mode: AddressingMode,
 }
 
 impl OpCode {
-    fn new(len:u8, cycles:u8, mode: AddressingMode) -> OpCode {
-        OpCode{
+    fn new(code: u8, len: u8, cycles: u8, mode: AddressingMode) -> OpCode {
+        OpCode {
+            code,
             len,
             cycles,
             mode,
         }
     }
 }
+
 pub enum ASM {
     ADC(OpCode),
     AND(OpCode),
@@ -88,15 +91,23 @@ pub enum ASM {
 }
 
 impl ASM {
-    pub fn compile_opcode(op_code:u8) -> ASM {
+    pub fn compile_opcode(op_code: u8) -> ASM {
         match op_code {
-            0x00 => ASM::BRK(OpCode::new(1, 7, AddressingMode::NoneAddressing)),
-            0xEA => ASM::NOP(OpCode::new(1, 2, AddressingMode::NoneAddressing)),
+            0x00 => ASM::BRK(OpCode::new(0x00, 1, 7, AddressingMode::NoneAddressing)),
+            0xEA => ASM::NOP(OpCode::new(0xEA, 1, 2, AddressingMode::NoneAddressing)),
 
-            0x69 => ASM::ADC(OpCode::new(2, 2, AddressingMode::Absolute)),
+            0x69 => ASM::ADC(OpCode::new(0x69, 2, 2, AddressingMode::Absolute)),
+            0x65 => ASM::ADC(OpCode::new(0x65, 2, 3, AddressingMode::ZeroPage)),
+            0x75 => ASM::ADC(OpCode::new(0x75, 2, 4, AddressingMode::ZeroPage_X)),
+            0x6d => ASM::ADC(OpCode::new(0x6d, 3, 4, AddressingMode::Absolute)),
+            0x7d => ASM::ADC(OpCode::new(0x7d, 3, 4/*+1 if page crossed*/, AddressingMode::Absolute_X)),
+            0x79 => ASM::ADC(OpCode::new(0x79, 3, 4/*+1 if page crossed*/, AddressingMode::Absolute_Y)),
+            0x61 => ASM::ADC(OpCode::new(0x61, 2, 6, AddressingMode::Indirect_X)),
+            0x71 => ASM::ADC(OpCode::new(0x71, 2, 5/*+1 if page crossed*/, AddressingMode::Indirect_Y)),
 
             _ => {
-            panic!()
-        } }
+                panic!()
+            }
+        }
     }
 }
