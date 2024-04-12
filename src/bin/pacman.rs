1ee91778891b9f5613d376bd7873c2dbb0202cdd
luexp <10457192@qq.com>
2024-04-12 23:48:47 +0800

    add pac

--- src/bin/pacman.rs
+++ src/bin/pacman.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use sdl2::event::Event;
+use sdl2::EventPump;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::{Color, PixelFormatEnum};
 use nes::bus::{Bus, Mem};
@@ -9,28 +10,27 @@ use nes::ppu::NesPPU;
 use nes::{joypad, render};
 use nes::render::frame::Frame;
 
-
 fn main() {
     // init sdl2
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
-        .window("Tile viewer", (256.0 * 4.0) as u32, (240.0 * 2.0) as u32)
+        .window("Tile viewer", (256.0 * 3.0) as u32, (240.0 * 3.0) as u32)
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
-    canvas.set_scale(2.0, 2.0).unwrap();
+    canvas.set_scale(3.0, 3.0).unwrap();
 
     let creator = canvas.texture_creator();
     let mut texture = creator
-        .create_texture_target(PixelFormatEnum::RGB24, 256 * 2, 240)
+        .create_texture_target(PixelFormatEnum::RGB24, 256, 240)
         .unwrap();
 
     //load the game
-    let bytes: Vec<u8> = std::fs::read("/Users/yifanjun/rust_proj/nes/nes/super.nes").unwrap();
+    let bytes: Vec<u8> = std::fs::read("/Users/yifanjun/rust_proj/nes/nes/pacman.nes").unwrap();
     let rom = Rom::new(&bytes).unwrap();
 
     let mut frame = Frame::new();
@@ -49,7 +49,7 @@ fn main() {
     // run the game cycle
     let bus = Bus::new(rom, move |ppu: &NesPPU, joypad: &mut joypad::Joypad| {
         render::render(ppu, &mut frame);
-        texture.update(None, &frame.data, 256 * 2 * 3).unwrap();
+        texture.update(None, &frame.data, 256 * 3).unwrap();
 
         canvas.copy(&texture, None, None).unwrap();
 
@@ -79,9 +79,7 @@ fn main() {
     });
 
     let mut cpu = CPU::new(bus);
+
     cpu.reset();
-    // cpu.run();
-    cpu.run_with_callback(|cpu| {
-        // println!("{}", trace(cpu));
-    });
+    cpu.run();
 }
